f1e84841904ee50ff2e3f1911ba9b3c08b9798a5
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 17:15:20 -0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Logo } from './components/logo/Logo'
-import { SectionHeader } from './components/sectionHeader/SectionHeader'
+import { Routes, Route } from 'react-router-dom'
+import { Logo } from './components/Logo'
+import { SectionHeader } from './components/SectionHeader'
 
 import { Home } from './pages/Home'
+import { AddToken } from './pages/AddToken'
 
 function App() {
   return (
     <>
       <Logo />
       <SectionHeader />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="addtoken" element={<AddToken />} />
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { theme } from './styles/theme'
 import App from './App'
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>,
   document.getElementById('root')
 )
