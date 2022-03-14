6232e47e3e382eaf0a484b786f9c6019a77d141b
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 14:30:03 -0300

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
+import { Flex, Text } from '@chakra-ui/react'
+
 export function Home() {
-  return <h1>hello world</h1>
+  return (
+    <Flex w="500px" mx="450" h="500px" mt="50">
+      <Flex justify="space-between" w="100%">
+        <div>
+          <Text color="text.primary">Tokens</Text>
+        </div>
+        <Text color="text.primary">Balance</Text>
+      </Flex>
+    </Flex>
+  )
 }
