acc96d2ed5328537bf6e0e03873e0123c3207bc3
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 14:30:27 -0300

    changes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Logo } from './components/logo/Logo'
+import { SectionHeader } from './components/sectionHeader/SectionHeader'
+
 import { Home } from './pages/Home'
 
 function App() {
   return (
     <>
+      <Logo />
+      <SectionHeader />
       <Home />
     </>
   )
