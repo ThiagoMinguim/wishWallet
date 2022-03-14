9bba6a4e4d6c7904b0a0a9b9961b67414784e185
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 12:58:06 -0300

    first settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from './styles/theme'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>,
   document.getElementById('root')
 )
