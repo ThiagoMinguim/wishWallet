b9c600dcf48339ab82a56f206c59e7eeb991c5e3
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 12:52:11 -0300

    add global theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    background: '#13152a',
+    button: {
+      confirm: '#aa33b5',
+      cancel: '#646464'
+    },
+    text: {
+      primary: '#ffffff'
+    }
+  },
+  fonts: {
+    heading: '"Roboto", sans-serif',
+    body: '"Roboto", sans-serif'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'background'
+      }
+    }
+  }
+})
