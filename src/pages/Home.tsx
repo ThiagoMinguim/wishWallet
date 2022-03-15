5346c37f703480897def6ce1ef4cfbfcd8af734d
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 21:16:30 -0300

    Add List in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
+import { MyWalletList } from '@/components/MyWalletList'
 import { Flex, Text } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
 
 export function Home() {
   return (
-    <Flex w="500px" mx="auto" h="300px" mt="50">
-      <Flex justify="space-between" w="100%">
-        <Flex>
-          <Text color="text.primary">Tokens</Text>
+    <>
+      <Flex w="500px" mx="auto" h="auto" mt="30" mb="5">
+        <Flex justify="space-between" w="100%">
+          <Flex>
+            <Text color="text.primary">Tokens</Text>
+          </Flex>
+
+          <Text color="text.primary">Balance</Text>
         </Flex>
-        <Text color="text.primary">Balance</Text>
       </Flex>
-    </Flex>
+      <MyWalletList />
+    </>
   )
 }
