614e9c84f91ebcd712687e07e23519af26da4509
Thiago Minguim <Chazy249@gmail.com>
2022-03-15 08:11:13 -0300

    making logo become clickable button to return to home

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,23 @@
 import { Flex, Image } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 import logoImg from '@/assets/logo.svg'
 
 export function Logo() {
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate('/')
+  }
+
   return (
     <Flex justify="center" mt="30px">
-      <Image w={['100', '200px']} src={logoImg} alt="klever logo" />
+      <Image
+        w={['100', '200px']}
+        src={logoImg}
+        alt="klever logo"
+        onClick={handleBack}
+        cursor="pointer"
+      />
     </Flex>
   )
 }
