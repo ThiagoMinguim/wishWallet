da75376c7c90c6c3b976741c2c425a06598f19ec
Thiago Minguim <Chazy249@gmail.com>
2022-03-14 21:14:46 -0300

    Instal react icons and types/node for import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.join(__dirname, 'src') }]
+  },
   plugins: [react()]
 })
